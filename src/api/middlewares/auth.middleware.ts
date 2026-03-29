3516146e464f01ca387281999d12ade4dc496543
Deisy Mariana Acosta Correa <deisymarcosta13@gmail.com>
2026-03-29 12:02:13 +0200

    feat: implementar middleware de autenticación con JWT para proteger rutas

--- src/api/middlewares/auth.middleware.ts
+++ src/api/middlewares/auth.middleware.ts
@@ -7,20 +7,20 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ mensaje: "No hay token" });
+    return res.status(401).json({ mensaje: "No hay token" });//valida si el token existe
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
 
-    const decoded = jwt.verify(token, env.JWT_SECRET);
+    const decoded = jwt.verify(token, env.JWT_SECRET);//verifica que el token sea valido usnado jwt_secret
 
-    (req as any).usuario = decoded;
+    (req as any).usuario = decoded;//guarda el usuario en la request
 
     next();
 
   } catch (error) {
     return res.status(401).json({ mensaje: "Token inválido" });
   }
-};
+}; 
